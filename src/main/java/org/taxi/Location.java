0a7646f93849af8275b2d731b8c28690756e00c6
its-ushen <ushen05@gmail.com>
2023-12-01 17:20:30 +0000

    worked on a node

--- src/main/java/org/taxi/Location.java
+++ src/main/java/org/taxi/Location.java
@@ -1,28 +1,39 @@
 package org.taxi;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Location {
-    private List<Location> neighbouringLocations;
-    private List<Taxi> containedTaxis;
-    public Location(List<Location> neighbouringLocations, List<Taxi> containedTaxis) {
-        this.neighbouringLocations = neighbouringLocations;
+    // attributes
+    private int x;
+    private int y;
+    private List<Location> neighbouringLocations = new ArrayList<>();
+    private List<Taxi> containedTaxis = new ArrayList<>();
+    
+    // constructor for location
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
     
-
-
+    // getters
     public List<Location> getNeighbouringLocations() {
         return neighbouringLocations;
     }
-    public void setNeighbouringLocations(List<Location> neighbouringLocations) {
-        this.neighbouringLocations = neighbouringLocations;
-    }
+
     public List<Taxi> getContainedTaxis() {
         return containedTaxis;
     }
-    public void setContainedTaxis(List<Taxi> containedTaxis) {
-        this.containedTaxis = containedTaxis;
+    
+    // methods to add to lsit
+    public void addTaxi (Taxi taxi) {
+        containedTaxis.add(taxi);
     }
+
+    public void generateNeighbouring(x, y) {
+        
+    }
+
 
     
 }
