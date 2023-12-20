3883c23ff3e25695b4551fefc4fb997f72773451
ruqn <59122094+ruanodowd@users.noreply.github.com>
2023-12-20 00:43:10 +0000

    Subclasses of Taxi

---
+++ src/main/java/org/taxi/taxi/NormalTaxi.java
@@ -0,0 +1,18 @@
+package org.taxi.taxi;
+
+import org.taxi.ActualMain;
+import org.taxi.pricing.PriceCalculator;
+import org.taxi.pricing.prices.PartyBusRate;
+import org.taxi.pricing.prices.TaxiRate;
+
+public class NormalTaxi extends Taxi{
+    TaxiRate taxiRate;
+    public NormalTaxi(String registrationNumber) {
+        super(registrationNumber);
+        this.taxiRate = ActualMain.priceCalculator.getPartyBusRate();
+    }
+
+    public TaxiRate getRate(){
+        return taxiRate;
+    }
+}
