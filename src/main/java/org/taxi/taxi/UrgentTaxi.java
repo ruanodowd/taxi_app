b47411b28d42239084900c32e7c3bd78b6e90e26
ruqn <59122094+ruanodowd@users.noreply.github.com>
2023-12-20 01:35:51 +0000

    TaxiRate

--- src/main/java/org/taxi/taxi/UrgentTaxi.java
+++ src/main/java/org/taxi/taxi/UrgentTaxi.java
@@ -5,15 +5,16 @@ import org.taxi.pricing.PriceCalculator;
 import org.taxi.pricing.prices.PartyBusRate;
 import org.taxi.pricing.prices.TaxiRate;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class UrgentTaxi extends Taxi {
     TaxiRate taxiRate;
-
     public UrgentTaxi(String registrationNumber) {
         super(registrationNumber);
-        this.taxiRate = ActualMain.priceCalculator.getPartyBusRate();
     }
 
-    public TaxiRate getRate() {
-        return taxiRate;
-    }
 }
